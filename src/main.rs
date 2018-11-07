15e2161e6017fa686fad88015f784c7f1f4ffd2b
Ryan Goldstein <ryanisaacg@gmail.com>
2018-11-07 15:46:01 -0500

    Return errors from main instead of panics, because it looks nicer

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,32 @@ use {
     clap::App,
     parser::parse,
     std::{
+        fmt::Display,
         fs::File,
         io::Read,
     },
 };
 
-fn main() {
+fn err_to_str<T>(result: Result<T, impl Display>) -> Result<T, String> {
+    result.map_err(|e| format!("{}", e))
+}
+
+fn main() -> Result<(), String> {
     let matches = App::new("budget")
         .version("0.1.0")
         .author("Ryan Goldstein")
         .about("Manage your money through space and time")
         .args_from_usage("<FILE>    'The file to read the budget from'")
         .get_matches();
-    let mut file = File::open(matches.value_of("FILE").unwrap()).unwrap();
+    let path = err_to_str(matches.value_of("FILE").ok_or("Missing required paramter file to read the budget from"))?;
+    let mut file = err_to_str(File::open(path))?;
     let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
+    err_to_str(file.read_to_string(&mut contents))?;
     let mut account = Account::new_root();
     for action in parse(&contents) {
-        account.apply(action.unwrap()).unwrap();
+        account.apply(action?)?;
     }
     println!("{}", account);
+    Ok(())
 }
 
