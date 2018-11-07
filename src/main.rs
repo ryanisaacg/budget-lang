19a061586486b380b5fe1eefc0edaf569af01f5b
Ryan Goldstein <ryanisaacg@gmail.com>
2018-11-07 15:29:09 -0500

    Fix some build warnings

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,12 @@ mod parser;
 use {
     account::Account,
     clap::App,
-    num_rational::Rational,
     parser::parse,
     std::{
         fs::File,
         io::Read,
     },
 };
-
-fn money_from(string: &str) -> Rational {
-    Rational::new((string.parse::<f32>().unwrap() * 100.0) as isize, 100)
-}
 
 fn main() {
     let matches = App::new("budget")
@@ -29,10 +24,10 @@ fn main() {
         .get_matches();
     let mut file = File::open(matches.value_of("FILE").unwrap()).unwrap();
     let mut contents = String::new();
-    file.read_to_string(&mut contents);
+    file.read_to_string(&mut contents).unwrap();
     let mut account = Account::new_root();
     for action in parse(&contents) {
-        account.apply(action.unwrap());
+        account.apply(action.unwrap()).unwrap();
     }
     println!("{}", account);
 }
